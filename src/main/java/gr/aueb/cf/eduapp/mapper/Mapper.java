3e92af0dc06f6b4533a30fe2ccc821c662017d1e
Sixtypounder <kaipanospanagiotis@gmail.com>
2026-04-20 19:52:20 +0300

    fix: mapper

--- src/main/java/gr/aueb/cf/eduapp/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/eduapp/mapper/Mapper.java
@@ -17,34 +17,5 @@ public class Mapper {
         return new UserReadOnlyDTO(user.getUuid().toString(), user.getUsername(), user.getRole().getName());
     }
 
-    public TeacherReadOnlyDTO mapToTeacherReadOnlyDTO(Teacher teacher) {
-        return new TeacherReadOnlyDTO(teacher.getUuid().toString(), teacher.getFirstname(), teacher.getLastname(),
-                teacher.getVat(), teacher.getRegion().getName());
-    }
-
-    public Teacher mapToTeacherEntity(TeacherInsertDTO dto) {
-        Teacher teacher = new Teacher();
-        teacher.setFirstname(dto.firstname());
-        teacher.setLastname(dto.lastname());
-        teacher.setVat(dto.vat());
-
-        UserInsertDTO userDTO = dto.userInsertDTO();
-        User user = new User();
-        user.setUsername(userDTO.username());
-        user.setPassword(userDTO.password());
-
-        teacher.addUser(user);
-
-        PersonalInfoInsertDTO personalInfoDTO = dto.personalInfoInsertDTO();
-        PersonalInfo personalInfo = new PersonalInfo();
-        personalInfo.setAmka(personalInfoDTO.amka());
-        personalInfo.setIdentityNumber(personalInfoDTO.identityNumber());
-        personalInfo.setPlaceOfBirth(personalInfoDTO.placeOfBirth());
-        personalInfo.setMunicipalityOfRegistration(personalInfoDTO
-                .municipalityOfRegistration());
-        teacher.setPersonalInfo(personalInfo);  // Set PersonalInfo entity to Teacher
-
-        return teacher;
-    }
 
 }
