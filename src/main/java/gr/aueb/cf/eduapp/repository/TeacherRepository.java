0073c04bedebad3301eaf795f8e8a3b0876c558c
Sixtypounder <kaipanospanagiotis@gmail.com>
2026-04-25 21:12:48 +0300

    clear

--- src/main/java/gr/aueb/cf/eduapp/repository/TeacherRepository.java
+++ src/main/java/gr/aueb/cf/eduapp/repository/TeacherRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 import java.util.UUID;
 
