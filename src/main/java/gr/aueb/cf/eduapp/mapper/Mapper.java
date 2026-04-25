099763c590702eafbbf17c0920ba9c3f807506e8
Sixtypounder <kaipanospanagiotis@gmail.com>
2026-04-25 20:57:44 +0300

    chore: insert into mapper teacher mapping to read only dto and teacher insert dto to teacher entity

--- src/main/java/gr/aueb/cf/eduapp/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/eduapp/mapper/Mapper.java
@@ -16,6 +16,35 @@ public class Mapper {
     public UserReadOnlyDTO mapToUserReadOnlyDTO(User user) {
         return new UserReadOnlyDTO(user.getUuid().toString(), user.getUsername(), user.getRole().getName());
     }
+    public TeacherReadOnlyDTO mapToTeacherReadOnlyDTO(Teacher teacher) {
+        return new TeacherReadOnlyDTO(teacher.getUuid().toString(), teacher.getFirstname(), teacher.getLastname(),
+                teacher.getVat(), teacher.getRegion().getName());
+    }
+
+    public Teacher mapToTeacherEntity(TeacherInsertDTO dto) {
+        Teacher teacher = new Teacher();
+        teacher.setFirstname(dto.firstname());
+        teacher.setLastname(dto.lastname());
+        teacher.setVat(dto.vat());
+
+        UserInsertDTO userDTO = dto.userInsertDTO();
+        User user = new User();
+        user.setUsername(userDTO.username());
+        user.setPassword(userDTO.password());
+
+        teacher.addUser(user);
+
+        PersonalInfoInsertDTO personalInfoDTO = dto.personalInfoInsertDTO();
+        PersonalInfo personalInfo = new PersonalInfo();
+        personalInfo.setAmka(personalInfoDTO.amka());
+        personalInfo.setIdentityNumber(personalInfoDTO.identityNumber());
+        personalInfo.setPlaceOfBirth(personalInfoDTO.placeOfBirth());
+        personalInfo.setMunicipalityOfRegistration(personalInfoDTO
+                .municipalityOfRegistration());
+        teacher.setPersonalInfo(personalInfo);  // Set PersonalInfo entity to Teacher
+
+        return teacher;
+    }
 
 
 }
